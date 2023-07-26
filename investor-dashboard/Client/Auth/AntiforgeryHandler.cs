244cc7e519aad498466ab8763c928b537ba4587c
Marc Lohrer <Marc.Lohrer@gmx.de>
2023-07-26 15:59:34 +0200

    initial setup

---
+++ investor-dashboard/Client/Auth/AntiforgeryHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace investordashboard.Client.Auth;
+public class AntiforgeryHandler : DelegatingHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        request.Headers.Add("X-CSRF", "1");
+        return base.SendAsync(request, cancellationToken);
+    }
+}
